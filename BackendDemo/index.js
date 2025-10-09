a8f67c08c683df7210a08eb400c3077cccf5e49d
Jithin Jose <007jithinjose@gmail.com>
2025-10-09 18:38:29 +0530

    Refactor backend server setup and enhance deployment workflow; update environment configurations for frontend and backend

--- BackendDemo/index.js
+++ BackendDemo/index.js
@@ -1,23 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import './config/database.js';
-import router from './router.js';
-import dotenv from 'dotenv';
+const express = require('express');
+const cors = require('cors');
+require('./config/database.js'); 
+const router = require('./router.js');
+const dotenv = require('dotenv');
 
+// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000; 
+
+// Middleware setup
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/demo',router);
+app.use('/', router);
 
+// Health check endpoint
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('GDG Backend Operational!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
