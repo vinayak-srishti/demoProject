61bba164d0dc4ce78d52705afb04a9af176bc736
RadhulSics <radhul.sics@gmail.com>
2025-10-11 09:17:30 +0530

    updated

--- FrontendDemo/src/components/UserNavbar/UserNavbar.jsx
+++ FrontendDemo/src/components/UserNavbar/UserNavbar.jsx
@@ -1,51 +1,57 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import img1 from '../../assets/images/logo2.png';
 import { Link, useNavigate } from 'react-router-dom';
 
 function UserNavbar() {
+  const navigate = useNavigate();
 
-    const navigate=useNavigate();
 
-    useEffect(()=>{
-        if(localStorage.getItem('userId'==null)){
-            navigate('/')
-        }
-    })
 
-    const handleLogout = () => {
-        localStorage.clear();
-        navigate("/");
-        window.location.reload(false);  
-      };
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate('/');
+    window.location.reload(false);
+  };
 
   return (
-    <div>
-       <nav className="navbar navbar-expand-lg navbar-dark bg-dark landing_custom_navbar pe-5">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark pe-5">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="#home">
+        <Link className="navbar-brand d-flex align-items-center" to="/user_home">
           <img
-            alt="Logo"
             src={img1}
+            alt="Logo"
             width="50"
             height="50"
-            className="d-inline-block align-top"
-          />{' '}
+            className="d-inline-block align-top me-2"
+          />
           LEGAL LIAISON
         </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+        {/* ✅ Correct Bootstrap 5 attributes */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav"> 
-          <ul className="navbar-nav ml-auto float-end">
+
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <Link onClick={handleLogout} className="nav-link" to="/user_add_complaint">Logout</Link>
+              <Link onClick={handleLogout} className="nav-link" to="#">
+                Logout
+              </Link>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-    </div>
-  )
+  );
 }
 
-export default UserNavbar
+export default UserNavbar;
